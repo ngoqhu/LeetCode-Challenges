dfac42be0d02ca855912825a03cecd9ccc9ce1fa
Hung Ngo Quang <79193361+ngoqhu@users.noreply.github.com>
2021-04-29 07:14:30 +0200

    Update twoSum.py

--- TwoSum/twoSum.py
+++ TwoSum/twoSum.py
@@ -1,1 +1,18 @@
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        result = 0
+        idxOfResults = []
+        
+        for i in range(len(nums)):
+            if result == 0:
+                result += nums[i]
+                idxOfResults.append(i)
+            else:
+                if result + nums[i] == target:
+                    idxOfResults.append(i)
+                    return idxOfResults
+                else:
+                    result = 0
+                    idxOfResults.clear()
 
+        return idxOfResults
