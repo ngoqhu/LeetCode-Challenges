ae0117d0a6bfd634f21f84bc80d47a4f5ab79454
Hung Ngo Quang <79193361+ngoqhu@users.noreply.github.com>
2021-04-30 09:21:06 +0200

    Update validParantheses.py

--- ValidParantheses/validParantheses.py
+++ ValidParantheses/validParantheses.py
@@ -1,1 +1,38 @@
+from collections import deque
 
+class Solution:
+    def isValid(self, s: str) -> bool:
+        stack = deque()
+
+        for i in range(len(s)):
+            if s[i] == "(" or s[i] == "[" or s[i] == "{":
+                stack.append(s[i])
+
+            if s[i] == ")":
+                if stack:               # Check if stack is not empty
+                    c = stack.pop()
+                    if c != "(":
+                        return False
+                else:
+                    return False
+
+            if s[i] == "]":
+                if stack:               # Check if stack is not empty
+                    c = stack.pop()
+                    if c != "[":
+                        return False
+                else:
+                    return False
+
+            if s[i] == "}":
+                if stack:               # Check if stack is not empty
+                    c = stack.pop()
+                    if c != "{":
+                        return False
+                else:
+                    return False
+
+        if stack:
+            return False
+
+        return True
