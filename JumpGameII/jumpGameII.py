b87f7b08039b72a0e43f71a9aa12155770b94e6e
Hung Ngo Quang <79193361+ngoqhu@users.noreply.github.com>
2021-05-08 22:57:02 +0200

    Update jumpGameII.py

--- JumpGameII/jumpGameII.py
+++ JumpGameII/jumpGameII.py
@@ -1,1 +1,16 @@
+class Solution:
+    def jump(self, nums: List[int]) -> int:
+        curr = -1
+        nxt = 0
+        result = 0
+        i = 0
 
+        while nxt < len(nums) - 1:
+            if i > curr:
+                result += 1
+                curr = nxt
+
+            nxt = max(nxt, nums[i] + i)
+            i += 1
+
+        return result
