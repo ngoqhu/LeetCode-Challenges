2d11a7c0822647d0aa14f7cdf951c1f5bbc93151
Hung Ngo Quang <79193361+ngoqhu@users.noreply.github.com>
2021-06-14 06:59:31 +0200

    Create solution.py

---
+++ SingleNumber/solution.py
@@ -0,0 +1,18 @@
+class Solution:
+    def singleNumber(self, nums: List[int]) -> int:
+        if len(nums) == 1:
+            return nums[0]
+
+        nums.sort()
+
+        for i in range(len(nums)):
+            if i + 1 == len(nums):
+                if nums[i] != nums[i-1]:
+                    return nums[i]
+
+            if i - 1 == -1:
+                if nums[i] != nums[i+1]:
+                    return nums[i]
+
+            if nums[i] != nums[i-1] and nums[i] != nums[i+1]:
+                return nums[i]
