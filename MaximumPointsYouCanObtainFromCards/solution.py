ef1d6158f1b6242836a99bca4fc2d48e00d32a45
Hung Ngo Quang <79193361+ngoqhu@users.noreply.github.com>
2021-06-18 06:13:03 +0200

    Create solution.py

---
+++ MaximumPointsYouCanObtainFromCards/solution.py
@@ -0,0 +1,10 @@
+class Solution:
+    def maxScore(self, cardPoints: List[int], k: int) -> int:
+        best = sum(cardPoints[:k])
+        total = sum(cardPoints[:k])
+        
+        for i in range(k - 1, -1, -1):
+            total += cardPoints[i + len(cardPoints) - k] - cardPoints[i]
+            best = max(best, total)
+        
+        return best
